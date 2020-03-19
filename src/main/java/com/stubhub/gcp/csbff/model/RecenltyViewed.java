165cf140bf9c1840415e045fa51d64adaea3e0e2
nnatesh <nnatesh@stubhub.com>
2020-03-19 10:20:15 -0700

    this is a poc for firebase data-store

---
+++ src/main/java/com/stubhub/gcp/csbff/model/RecenltyViewed.java
@@ -0,0 +1,36 @@
+package com.stubhub.gcp.csbff.model;
+
+public class RecenltyViewed {
+
+
+    private String userId;
+    private String eventId;
+    private String timestamp;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+
+}
